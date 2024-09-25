2bf07841df145cc1ed83311b480551da4a3a4e03
jpnonded <jeanphilippe.nondedeo@orange.com>
2024-09-25 15:01:52 +0200

    tuto angular

---
+++ src/app/toto/toto.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-toto',
+    standalone: true,
+    templateUrl: './toto.component.html',
+    styleUrl: './toto.component.scss'
+})
+export class TotoComponent {
+
+
+}
