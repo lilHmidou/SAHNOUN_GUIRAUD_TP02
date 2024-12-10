6b043a21ceb2108af61e259d1c2ee2bf55e83306
lilHmidou <ahmedaminesahnoun@gmail.com>
2024-12-10 18:18:31 +0100

    add contact page and form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.contactForm = this.fb.group({
+      firstName: ['',Validators.required],
+      lastName: ['',Validators.required],
+      age: [''],
+      hideEmail: [false],
+      email: ['',[Validators.required, Validators.email]],
+      comment: ['',Validators.required],
+    });
+}
+
+  toggleEmailValidation() {
+    const emailControl = this.contactForm.get('email');
+    const hideEmail = this.contactForm.get('hideEmail')?.value;
+
+    if (hideEmail) {
+      emailControl?.clearValidators();
+    } else {
+      emailControl?.setValidators([Validators.required, Validators.email]);
+    }
+    emailControl?.updateValueAndValidity(); // Met à jour la validation
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      alert('Le formulaire est valide');
+      this.router.navigate(['/']);
+    }
+  }
 }
